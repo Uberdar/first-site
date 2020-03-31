8203951975f19fc2a50969b814f4915fd6163583
Uberdar <allexvik@gmail.com>
2020-03-31 18:12:00 +0300

    version 1.1

--- README.md
+++
@@ -1,3 +0,0 @@
-# first-site
-this was a first try
-main page uses mainpage.css and other pages use index.css
--- gallery.js
+++
@@ -1,9 +0,0 @@
-function upDate(previewPic){
-  document.getElementById('image').style.backgroundImage = "url("+ previewPic.src +")";
-    document.getElementById('image').innerHTML = "";
-	}
-
-	function unDo(){
-   document.getElementById('image').style.backgroundImage = "url()";
-   document.getElementById('image').innerHTML = "Hover over an image to display here.";
-	}
